9fe6bdd32acc8d686ead3131866508ae5cfccb06
Filipe Herculano <fifo_@live.com>
2019-12-15 17:25:36 -0500

    Update RepoCard to display reop's language
    Improve styles and fix typo

--- src/components/RepoCard.jsx
+++ src/components/RepoCard.jsx
@@ -1,15 +1,42 @@
 import React from 'react'
-import { Card } from 'react-bootstrap'
+import { Card, Badge } from 'react-bootstrap'
 import PropTypes from 'prop-types'
+import githubIcon from '../github.svg'
 
 const RepoCard = ({ repo }) => {
   return (
     <Card
-      style={{ minWidth: '18rem', minHeight: '13rem' }}
-      className="m-2 custom-card custom-borders"
+      style={{
+        minWidth: '18rem',
+        minHeight: '13rem',
+      }}
+      className="m-sm-2 custom-card custom-borders"
     >
       <Card.Body>
-        <Card.Title>{repo.name}</Card.Title>
+        <Card.Title>
+          <u>{repo.name}</u>
+          <span>
+            <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
+              <img
+                alt="GitHub icon"
+                src={githubIcon}
+                className="ml-2 mr-2"
+                style={{ maxWidth: '1.2rem' }}
+              />
+            </a>
+          </span>
+          <Badge
+            className="small"
+            style={{
+              color: repo.language_color,
+              border: `.5px solid ${repo.language_color}`,
+              boxShadow: `1px 1px 0 0 ${repo.language_color}`,
+              borderRadius: '1px',
+            }}
+          >
+            {repo.language}
+          </Badge>
+        </Card.Title>
         <Card.Text>{repo.description}</Card.Text>
       </Card.Body>
     </Card>
